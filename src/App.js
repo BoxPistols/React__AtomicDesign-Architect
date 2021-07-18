e7d21423ac6e6587ebbb73b4e05907adc09f22a9
A_ito <BoxPistols@users.noreply.github.com>
2021-07-18 22:13:44 +0900

    separate base files Separate to 1-Container /  2-Presentational

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-import "./styles.css";
-import { Button } from "./compontnts/Button";
-import { connect } from "react-redux";
+// import "./styles.css";
+// import { Button } from "./compontnts/Button";
 
-const App = ({ num, plus, minus }) => {
-  return (
-    <div>
-      <div className="App">
-        <h1>Hello Redux</h1>
-        <h2>{num}</h2>
-        <Button child={"-"} onClick={() => minus(10)} />
-        <Button child={"+"} onClick={() => plus(25)} />
-      </div>
-    </div>
-  );
-};
-
-const mapStateToProps = (state) => {
-  return { num: state };
-};
-
-const mapDiapatchToProps = (diapatch) => {
-  return {
-    plus: (numCalc) => {
-      diapatch({ type: "PLUS", payload: { num: numCalc } });
-    },
-    minus: (numCalc) => {
-      diapatch({ type: "MINUS", payload: { num: numCalc } });
-    }
-  };
-};
-export default connect(mapStateToProps, mapDiapatchToProps)(App);
+// export const App = ({ num, plus, minus }) => {
+//   return (
+//     <div>
+//       <div className="App">
+//         <h1>Hello Redux</h1>
+//         <h2>{num}</h2>
+//         <Button child={"-"} onClick={() => minus(10)} />
+//         <Button child={"+"} onClick={() => plus(25)} />
+//       </div>
+//     </div>
+//   );
+// };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import { reducer } from "./reducer";
 
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import  App  from "./Redux/Logic/Connect";
 
 const store = createStore(reducer);
 
